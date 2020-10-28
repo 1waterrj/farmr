728cbeb1d8c7ac0944f3da103ab8185746765e48
Jordan Waters <jwaters@myorderwrite.com>
2020-10-27 20:33:16 -0400

    some shit

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+app.config.from_object('config')
 
 if __name__ == "__main__":
     app.run()
