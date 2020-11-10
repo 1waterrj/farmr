c7ee2687c76fc3b682cd94f25420d26236c57b79
Jordan Waters <jwaters@myorderwrite.com>
2020-11-10 10:55:08 -0500

    updating template for farmr

--- application/models.py
+++ application/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.dialects import postgresql
+import uuid
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -8,4 +10,16 @@ migrate = Migrate()
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
     email = db.Column(db.String, unique=True, nullable=False)
+
+class Input(db.Model):
+    __tablename__ = "inputs"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique)
+class MqttEvents(db.Model):
+    __tablename__ = "mqtt_events"
+    id = db.Column(db.Integer, primary_key=True)
+
+    fingerprint = db.Column(postgresql.UUID(as_uuid=True), nullable=False)
