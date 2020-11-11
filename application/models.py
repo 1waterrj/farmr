ee49446a89374c2caed94a8e5246c36c610c8b0f
Jordan Waters <jwaters@myorderwrite.com>
2020-11-10 20:53:19 -0500

    farmr schema

--- application/models.py
+++ application/models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.schema import ForeignKeyConstraint
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -15,67 +17,66 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=db.func.now())
 
 
-class Input(db.Model):
-    __tablename__ = "inputs"
-    __table_args__ = (
-        db.ForeignKeyConstraint(
-            ['name'],
-            ['zone_id'],
-        ),
-    )
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    zone_id = db.Column(db.Integer, db.ForeignKey('Zone.id'))
-    fingerprint = db.Column(postgresql.UUID(as_uuid=True), nullable=False)
-    created_at = db.Column(db.DateTime, default=db.func.now())
-
-
-class Zone(db.Model):
-    __tablename__ = "zones"
-    __table_args__ = (
-        db.ForeignKeyConstraint(
-            ['name'],
-            ['group_id'],
-        ),
-    )
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    group_id = db.Column(db.Integer, db.ForeignKey('Group.id'))
-    created_at = db.Column(db.DateTime, default=db.func.now())
-
-
 class Location(db.Model):
     __tablename__ = "locations"
-    __table_args__ = (
-        db.ForgeignKeyConstraint(
-            ['name'],
-            ['user_id'],
-        ),
-    )
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     zipcode = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=db.func.now())
+    groups = relationship("Group", backref="locations")
+    __table_args__ = (
+        db.UniqueConstraint(
+            'name',
+            'user_id',
+            name='_location_user_uc'
+        ),
+    )
 
 
 class Group(db.Model):
     __tablename__ = "groups"
-    __table_args__ = (
-        db.ForeignKeyConstraint(
-            ['name'],
-            ['location_id'],
-        ),
-    )
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    location_id = db.Column(db.Integer, db.ForeignKey('Location.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey(Location.id))
     created_at = db.Column(db.DateTime, default=db.func.now())
+    __table_args__ = (db.UniqueConstraint('name', 'location_id', name='_group_location_uc'),)
+
+
+class Zone(db.Model):
+    __tablename__ = "zones"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    group_id = db.Column(db.Integer, db.ForeignKey(Group.id))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    __table_args__ = (
+        db.UniqueConstraint(
+            'name',
+            'group_id',
+            name='_zone_group_uc'
+        ),
+    )
+
+
+class Input(db.Model):
+    __tablename__ = "inputs"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    zone_id = db.Column(db.Integer, db.ForeignKey(Zone.id))
+    fingerprint = db.Column(postgresql.UUID(as_uuid=True), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    __table_args__ = (
+        db.UniqueConstraint(
+            'name',
+            'zone_id',
+            name='_input_zone_uc'
+        ),
+    )
 
 
 class MqttEvents(db.Model):
     __tablename__ = "mqtt_events"
     id = db.Column(db.Integer, primary_key=True)
-    input_id = db.Column(db.Integer, db.ForeignKey('Input.id'))
+    input_id = db.Column(db.Integer, db.ForeignKey(Input.id))
     data_load = db.Column(db.JSON, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
