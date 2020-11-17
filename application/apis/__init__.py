d2df86452e7c4250656db410f380e9a716f21d9b
Jordan Waters <jwaters@myorderwrite.com>
2020-11-17 08:47:23 -0500

    bad code

--- application/apis/__init__.py
+++ application/apis/__init__.py
@@ -1,13 +1,13 @@
-from farmr import Api
+from flask_restx import Api
 
-from .farmr1 import api as ns1
+from application.apis.farmr1 import api as ns1
 
+print("this is the init file")
 api = Api(
-    title='farmr',
-    version='1.0',
-    description='api for managing all resources related to farmr',
+    title="farmr",
+    version="1.0",
+    description="api for managing all resources related to farmr",
     # All API metadatas
 )
 
 api.add_namespace(ns1)
-
--- application/app.py
+++ application/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask_restx import Api
+from application.apis import api
+from flask_restx import Api, Namespace, Resource, fields
 
 
 def create_app(config_name):
 
     app = Flask(__name__)
-    api = Api()
-    api.init_app(app)
 
     config_module = f"application.config.{config_name.capitalize()}Config"
 
@@ -17,8 +16,48 @@ def create_app(config_name):
     db.init_app(app)
     migrate.init_app(app, db)
 
-    @app.route("/")
-    def hello_world():
-        return "Hello, World!"
+    api.init_app(app)
+    print(api, "this is the app")
+    cat = api.model(
+        "Cat",
+        {
+            "id": fields.String(required=True, description="The cat identifier"),
+            "name": fields.String(required=True, description="The cat name"),
+        },
+    )
+
+    CATS = [
+        {"id": "felix", "name": "Felix"},
+        {"id": "tom", "name": "Tom"},
+    ]
+
+    print(api, "this is the farmr file")
+
+    @api.route("/")
+    class CatList(Resource):
+        @api.doc("list_cats")
+        @api.marshal_list_with(cat)
+        def get(self):
+            print(CATS)
+            """List all cats"""
+            return CATS
+
+    @api.route("/<id>")
+    @api.param("id", "The cat identifier")
+    @api.response(404, "Cat not found")
+    class Cat(Resource):
+        @api.doc("get_cat")
+        @api.marshal_with(cat)
+        def get(self, id):
+            """Fetch a cat given its identifier"""
+            for cat in CATS:
+                if cat["id"] == id:
+                    return cat
+            api.abort(404)
+
+    @api.route("/hello")
+    class HelloWorld(Resource):
+        def get(self):
+            return {"hello": "world"}
 
     return app
